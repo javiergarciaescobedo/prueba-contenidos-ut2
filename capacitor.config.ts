5b8298063f143988d56b83eb9ef30f3b95d2d671
Javier García Escobedo <javiergarbedo@gmail.com>
2025-10-02 19:46:52 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'prueba-contenidos-ut2',
+  webDir: 'www'
+};
+
+export default config;
